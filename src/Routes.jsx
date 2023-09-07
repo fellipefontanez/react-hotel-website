b43881a0e6dacf55153efeb458ac15e84e6678ad
Fellipe Fontanez <fellipefontanez@gmail.com>
2023-09-06 23:24:36 -0300

    feature: adicionando preview host

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,32 +1,30 @@
-
-import {BrowserRouter, Route, Routes, Outlet} from 'react-router-dom';
-import Template from './pages/Template';
-import Home from './pages/Home';
-import Room from './pages/Room';
-import LoginForm from './pages/Login-Form';
-import {Navigate} from 'react-router-dom';
-import {createContext, useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Template from "./pages/Template";
+import Home from "./pages/Home";
+import Room from "./pages/Room";
+import LoginForm from "./pages/Login-Form";
+import { Navigate } from "react-router-dom";
+import { createContext, useState } from "react";
 
 export const SearchContext = createContext();
-
 
 const AppRouter = () => {
   const [search, setSearch] = useState("");
 
   return (
-    <SearchContext.Provider value={{search,setSearch}}>
+    <SearchContext.Provider value={{ search, setSearch }}>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Template />}>
+          <Route path="/" element={<Template />}>
             <Route index element={<Home />} />
-            <Route path='/room' element={<Room />} />
-            <Route path='profile' element={<LoginForm />} />
-            <Route path='*' element={<Navigate to="/" />} />
+            <Route path="/room" element={<Room />} />
+            <Route path="profile" element={<LoginForm />} />
+            <Route path="*" element={<Navigate to="/" />} />
           </Route>
         </Routes>
       </BrowserRouter>
     </SearchContext.Provider>
   );
-}
+};
 
 export default AppRouter;
