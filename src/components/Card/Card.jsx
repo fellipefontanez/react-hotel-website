6df11e94805558891878c884cda4873c05e494d1
Fellipe Fontanez <fellipefontanez@gmail.com>
2023-09-06 23:04:23 -0300

    refactor: componentização do Card

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function Card({ room, position }) {
+  const navigate = useNavigate();
+  const handleCard = (link) => {
+    navigate(link);
+  };
+  return (
+    <div
+      key={room.location + room.tags + position}
+      onClick={() => handleCard("room")}
+      className="container"
+    >
+      <img
+        className="imgRoom houston"
+        src={room.image}
+        alt="Room"
+      />
+      <div className="content-container">
+        <p className="city">{room.location}</p>
+        <div className="tags">
+          <ul>
+            {room.tags.map((tag, index) => (
+              <li key={tag + index}>
+                {index !== 0 && <span>•</span>}
+                {tag}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="price">
+          <div className="value">${room.price}</div>
+          <div className="per">Day</div>
+        </div>
+      </div>
+    </div>
+  );
+}
