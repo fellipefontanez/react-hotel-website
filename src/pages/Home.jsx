30701835d729349cfaf2e446c6f5a2e84b921ca0
Fellipe Fontanez <fellipefontanez@gmail.com>
2023-09-06 23:04:43 -0300

    refactor: componentização do card

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,73 +1,57 @@
 import axios from "axios";
-import {useContext , useEffect, useState} from "react";
-import {SearchContext} from "../Routes";
-import { useNavigate } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { SearchContext } from "../Routes";
+import Card from "../components/Card/Card";
 
 // URL's dos dados
-const dataUrl = "https://raw.githubusercontent.com/fellipefontanez/mockup/main/fakedb.json";
+const dataUrl =
+  "https://raw.githubusercontent.com/fellipefontanez/mockup/main/fakedb.json";
 
 const Home = () => {
-  
-  const navigate = useNavigate();
-
   const [quarto, setQuarto] = useState([]);
-  
-  const {search} = useContext(SearchContext);
+
+  const { search } = useContext(SearchContext);
   const [filtredList, setFiltredList] = useState(quarto);
   let effectSearch;
 
-  useEffect(()=>{
-    axios.get(dataUrl)
-      .then(res => {
-        setQuarto(res.data)
-      })
-  }, [])
+  useEffect(() => {
+    axios.get(dataUrl).then((res) => {
+      setQuarto(res.data);
+    });
+  }, []);
 
-    useEffect(() => {
-      clearTimeout(effectSearch);
-      const searchList = quarto.filter((room) => {
-        switch (true) {
-          case room.location.toUpperCase().includes(search.toUpperCase()):
-            return true;
-          case room.price <= Number(search):
-            return true;
-          case room.tags.some((tag) => tag.toUpperCase().includes(search.toUpperCase())):
-            return true;
-          default:
-            return false;
-        }
-      });
-      effectSearch = setTimeout(() => setFiltredList(searchList), 600);
-    },[search,quarto]);
+  useEffect(() => {
+    clearTimeout(effectSearch);
+    const searchList = quarto.filter((room) => {
+      switch (true) {
+        case room.location.toUpperCase().includes(search.toUpperCase()):
+          return true;
+        case room.price <= Number(search):
+          return true;
+        case room.tags.some((tag) =>
+          tag.toUpperCase().includes(search.toUpperCase())
+        ):
+          return true;
+        default:
+          return false;
+      }
+    });
+    effectSearch = setTimeout(() => setFiltredList(searchList), 600);
+  }, [search, quarto]);
 
-    const handleCard = (link) => {
-      navigate(link);
-    }
-
-    //Page's body
-    return (
-      <div className="main">
-      {filtredList.map((room,index) => (
-        <div key={room.location + room.tags + index} onClick={() => handleCard('room')} className="container">
-        <img className="imgRoom houston" src={room.image} alt="Room" />
-        <div className="content-container">
-          <p className="city">{room.location}</p>
-          <div className="tags">
-            <ul>
-              {room.tags.map((tag ,index) => (
-                  <li key={tag + index}>{index !== 0 && <span>•</span>}{tag}</li>
-              ))}
-            </ul>
-          </div>
-          <div className="price">
-            <div className="value">${room.price}</div>
-            <div className="per">Day</div>
-          </div>
-        </div>
-      </div>
-      ))}
-    </div>
-    )
-}
+  return (
+    <main className="main">
+      <section className="cards-container">
+        {filtredList.map((room, index) => (
+          <Card
+            room={room}
+            position={index}
+            key={index}
+          />
+        ))}
+      </section>
+    </main>
+  );
+};
 
 export default Home;
