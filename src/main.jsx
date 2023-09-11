964f1247c7f677548c5b29ae9efb70a788d715d9
fellipefontanez <fellipefontanez@gmail.com>
2023-09-11 16:52:33 -0300

    removing versel

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import AppRouter from "./Routes";
 import "./index.css";
-import { inject } from "@vercel/analytics";
-
-inject();
 
 const mainContent = document.getElementById("mainContent");
 const rootMain = ReactDOM.createRoot(mainContent);
